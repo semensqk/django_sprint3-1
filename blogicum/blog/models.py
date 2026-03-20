4f8f0aa65c3ba04cd04eda9199db10ea68335dd9
Eugene Kozhushko <kozhushko2006@gmail.com>
2026-03-20 21:40:56 +0500

    upd

---
+++ blogicum/blog/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class AbstracModel(models.Model):
+    is_published = models.BooleanField(
+        blank=False,
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        blank=False,
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        abstract = True
+
+User = get_user_model()
+
+
+class Category(AbstracModel):
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name='Заголовок'
+    )
+    description = models.TextField(
+        blank=False,
+        verbose_name='Описание'
+    )
+    slug = models.SlugField(
+        blank=False,
+        verbose_name='Идентификатор',
+        help_text='Идентификатор страницы для URL; '
+                  'разрешены символы латиницы, цифры, дефис и подчёркивание.',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+
+class Location(AbstracModel):
+    name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name='Название места'
+    )
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+
+class Post(AbstracModel):
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name='title'
+    )
+    text = models.TextField(
+        blank=False,
+        verbose_name='Текст'
+    )
+    pub_date = models.DateTimeField(
+        blank=False,
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в '
+                  'будущем — можно делать отложенные публикации.'
+    )
+    author = models.ForeignKey(
+        User,
+        blank=False,
+        on_delete=models.CASCADE,
+        null=False,
+        verbose_name='Автор публикации'
+    )
+    location = models.ForeignKey(
+        Location,
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Местоположение'
+    )
+    category = models.ForeignKey(
+        Category,
+        blank=False,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
---
+++ blogicum/blog/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+
+from datetime import datetime
+
+from .models import Post, Category
+
+
+def index(request):
+    template_name = 'blog/index.html'
+    post_list = Post.objects.select_related(
+        'category'
+    ).filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+    ).order_by(
+        '-pub_date'
+    )[0:5]
+
+    context = {
+        'post_list': post_list
+    }
+    return render(request, template_name, context)
+
+
+def post_detail(request, id):
+    template_name = 'blog/detail.html'
+    user_post = Post.objects.select_related(
+        'category'
+    ).filter(
+        pub_date__lte=datetime.now(),
+        is_published=True,
+        category__is_published=True
+    )
+    post = get_object_or_404(user_post, pk=id)
+    
+    context = {
+        'post': post
+    }
+    return render(request, template_name, context)
+
+
+def category_posts(request, category_slug):
+    template_name = 'blog/category.html'
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    post_list = Post.objects.select_related(
+        'category'
+    ).filter(
+        category=category.pk,
+        is_published=True,
+        pub_date__lte=datetime.now()
+    )
+
+    context = {
+        'category': category,
+        'post_list': post_list
+    }
+    return render(request, template_name, context)
